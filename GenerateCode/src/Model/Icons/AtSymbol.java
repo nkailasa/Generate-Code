4ce7cab1f22dc9ff487eb7b5907651f6830c2f2c
Isaac Beale <isaacdbeale@gmail.com>
2021-03-27 16:52:28 -0700

    override the isIconValid function for the @ symbol

--- GenerateCode/src/Model/Icons/AtSymbol.java
+++ GenerateCode/src/Model/Icons/AtSymbol.java
@@ -1,6 +1,7 @@
 package Model.Icons;
 
 import java.io.Serial;
+import java.util.ArrayList;
 
 public class AtSymbol extends IconParent {
     @Serial
@@ -8,5 +9,29 @@ public class AtSymbol extends IconParent {
 
     public AtSymbol() {
         super(2, 2, Constants.AT);
+    }
+
+    @Override
+    public boolean isIconValid(){
+        boolean isLoopConditionFilled = false;
+        ArrayList<IconParent> visitedIcons = new ArrayList<IconParent>();
+        visitedIcons.add(this);
+        int count = 0;
+        while(count < visitedIcons.size()){
+            for(IconParent output : visitedIcons.get(count).outputs){
+                if(output == this){
+                    isLoopConditionFilled = true;
+                    break;
+                }
+                else{
+                    if(!visitedIcons.contains(output)){
+                        visitedIcons.add(output);
+                    }
+                }
+            }
+            count++;
+        }
+        return(super.isIconValid() && isLoopConditionFilled);
+
     }
 }
